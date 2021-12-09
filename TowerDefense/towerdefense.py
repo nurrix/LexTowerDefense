82b8bb30de5afb294875ccf8daf4144379d7e562
Nurrix <nurrix@gmail.com>
2021-12-09 20:00:50 +0100

    Added initializer of factory into the factory List

--- TowerDefense/towerdefense.py
+++ TowerDefense/towerdefense.py
@@ -10,12 +10,12 @@ def main() -> None:
         This function starts our program.
     """
     # Test by creating a Tower
-    factory = {"SimpleTower": SimpletonTowerFactory(),
+    unit_factory = {"SimpleTower": SimpletonTowerFactory(),
                "SplashTower": SplashTowerFactory(),
             }
     
-    simpletower = factory["SimpleTower"].create_new_unit(Loc=Vector2D(x=100, y=313))
-    splashtower = factory["SplashTower"].create_new_unit(Loc=Vector2D(x=100, y=313))
+    simpletower = unit_factory["SimpleTower"].create_new_unit(Loc=Vector2D(x=100, y=313))
+    splashtower = unit_factory["SplashTower"].create_new_unit(Loc=Vector2D(x=100, y=313))
     print(simpletower)
     print(splashtower)
     
